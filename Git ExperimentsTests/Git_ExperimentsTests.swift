2e0f2e849c1eacf8ee517f6589d03f33485cc173
Küng Hansueli <system-shock@web.de>
2024-11-10 13:51:07 +0100

    Initial Commit

---
+++ Git ExperimentsTests/Git_ExperimentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ExperimentsTests.swift
+//  Git ExperimentsTests
+//
+//  Created by Küng Hansueli on 10.11.2024.
+//
+
+import Testing
+@testable import Git_Experiments
+
+struct Git_ExperimentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
